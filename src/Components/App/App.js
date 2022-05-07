3c82d4373ca5755a3a1223c2e704ffd673e82a16
Hirad Fazeli <82698542+hiradfazeli@users.noreply.github.com>
2022-05-07 16:23:53 +0400

    Save a User's Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
-import { Spotify } from '../../util/Spotify'
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,17 +14,9 @@ class App extends React.Component {
     this.savePlaylist = this.savePlaylist.bind(this);
     this.search = this.search.bind(this);
     this.state = {
-        searchResults: [
-        {name: 'name1', artist: 'artist1', album: 'album1', id: 1,},
-        {name: 'name2', artist: 'artist2', album: 'album2', id: 2,},
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3,}
-      ],
-      playlistName: 'Playlist 1',
-      playlistTracks: [
-        {name: 'Playlist1', artist: 'artist1', album: 'album1', id: 4},
-        {name: 'Playlist2', artist: 'artist2', album: 'album2', id: 5},
-        {name: 'Playlist3', artist: 'artist3', album: 'album3', id: 6}
-      ]
+        searchResults: [],
+      playlistName: 'My Playlist',
+      playlistTracks: []
     }
   }
 
@@ -49,6 +41,13 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs
+      ).then(() => {
+        this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        })
+      });
   }
 
   search(term) {
@@ -62,7 +61,7 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar onSearch={this.seacrh} />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,5 @@
-import { SearchBar } from "../Components/SearchBar/SearchBar";
-
 let accessToken;
-const clientId = '337ed06aa291451a99db7acdc631c4d4';
+const clientId = 'Your client ID from Spotify';
 const redirectUri = 'http://localhost:3000/';
 
 const Spotify = {
@@ -27,7 +25,7 @@ const Spotify = {
     },
 
     search(term) {
-        const accessToken = this.getAccessToken();
+        const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {Authorization: `Bearer ${accessToken}`} 
         }).then(response => {
@@ -43,6 +41,36 @@ const Spotify = {
                 album: track.album,
                 uri: track.uri
             }))
+        })
+    },
+
+    savePlaylist(playlistName, trackURIs) {
+        if(!(playlistName || trackURIs.length)) {
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = {Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: playlistName})
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({uris: trackURIs})
+                })
+            })
         })
     }
 }
